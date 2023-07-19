ac3f608bc2d32c26932101fac04de28eadfef585
genkoskarev <gen.koskarev@gmail.com>
2023-07-20 00:50:15 +0300

    add validation descr and img. add rus file valid

--- resources/views/posts/parts/form.blade.php
+++ resources/views/posts/parts/form.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
-    <input name="title" type="text" class="form-control" required value="{{ $post->title ?? '' }}">
+    <input name="title" type="text" class="form-control" required value="{{ old('title') ??$post->title ?? '' }}">
 </div>
 <div class="form-group">
-    <textarea name="descr" class="form-control" rows="3" required>{{ $post->descr ?? '' }}</textarea>
+    <textarea name="descr" class="form-control" rows="3" required>{{ old('descr') ??$post->descr ?? '' }}</textarea>
 </div>
 <div class="form-group">
     <input name="img" type="file" id="">
