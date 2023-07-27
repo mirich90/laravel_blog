4f04d781d12b7b77274a92a72969c9cf7643fdb1
genkoskarev <gen.koskarev@gmail.com>
2023-07-28 00:28:02 +0300

    add data created nd updated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,9 @@
             <div class="card">
                 <div class="card-header">
                     <h2>{{ $post->title }}</h2>
-                    <div class="card-author">Автор: {{ $post->name }}</div>
+                    <div class="card-author"><b>Автор</b>: {{ $post->name }}</div>
+                    <div class="card-data"><b>Дата создания</b>: {{ $post->created_at }}</div>
+                    <div class="card-data"><b>Дата редактирования</b>: {{ $post->updated_at }}</div>
                     <div class="card-date">{{ $post->created_at->diffForHumans() }}</div>
                 </div>
                 <div class="card-body">
